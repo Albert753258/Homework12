9943303ac7101ba8ee1b53f7189fb06461149e0d
Albert <albert.nasyrov2016@gmail.com>
2020-02-03 22:56:02 +0300

    Для последовательности чисел, заканчивающичся -1, вывести суммучисел, произведение цифр которых делится на 3

---
+++ Homework4/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sumOfNumbers  = 0;
+        int current1 = 0;
+        int current;
+        current1 = scanner.nextInt();
+        while (current1 != -1){
+            int sum = 0;
+            for(current = current1; current != 0; current = current / 10){
+                sum = sum + current % 10;
+            }
+            if(sum % 3 == 0){
+                sumOfNumbers = sumOfNumbers + current1;
+            }
+            else{
+            }
+            current1 = scanner.nextInt();
+        }
+        System.out.println(sumOfNumbers);
+    }
+}
