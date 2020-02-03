b49c7c266a6fffcb75bad418ea3dc92897e56062
Albert <albert.nasyrov2016@gmail.com>
2020-02-03 22:58:20 +0300

    Для последовательности чисел, заканчивающичся -1, вывести суммучисел, произведение цифр которых делится на 3

--- Homework4/src/Main.java
+++ Homework4/src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         int current;
         current1 = scanner.nextInt();
         while (current1 != -1){
-            int sum = 0;
+            int sum = 1;
             for(current = current1; current != 0; current = current / 10){
-                sum = sum + current % 10;
+                sum = sum * (current % 10);
             }
             if(sum % 3 == 0){
                 sumOfNumbers = sumOfNumbers + current1;
