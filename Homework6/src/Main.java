3284ef89365eca497c4479c1e95071d1e8c0bf70
Albert <albert.nasyrov2016@gmail.com>
2020-02-10 17:38:32 +0300

    Без использования String

--- Homework6/src/Main.java
+++ Homework6/src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int[] a = {3, 2, 1, 6, 1};
-        String number = "";
+        int number = 0;
+        int length = a.length - 1;
         for (int i = 0; i < a.length; i ++) {
-            number = number + a[i];
+            number = number + degree(a[i], length);
+            length--;
         }
         System.out.println(number);
+    }
+    public static int  degree(int a, int b){
+        while(b > 0){
+            a = a * 10;
+            b --;
+        }
+        return a;
     }
 }
